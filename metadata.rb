6e287882a6cebc8a6bc2c6888b6d001f72a6330f
William Notowidagdo <wnotowidagdo@gmail.com>
2019-03-28 13:52:04 +0700

    satisfy foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,1 +1,21 @@
 name 'opsworks_cookbook_demo'
+maintainer 'John Doe'
+maintainer_email 'john@example.com'
+license 'MIT'
+description 'Installs/Configures opsworks_cookbook_demo'
+long_description 'Installs/Configures opsworks_cookbook_demo'
+version '0.1.0'
+chef_version '>= 13.0'
+supports 'ubuntu'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+issues_url 'https://github.com/williamn/opsworks_cookbook_demo/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+source_url 'https://github.com/williamn/opsworks_cookbook_demo'
